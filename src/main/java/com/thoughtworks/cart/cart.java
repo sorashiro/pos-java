3e27a8e3af08cbc37257b984996d4e8b8bed4cc4
sorashiro <yufei930101@gmail.com>
2014-10-08 18:15:46 +0800

    update get barcodes function

--- src/main/java/com/thoughtworks/cart/cart.java
+++ src/main/java/com/thoughtworks/cart/cart.java
@@ -1,21 +1,22 @@
 package com.thoughtworks.cart;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class cart {
-    public static String getBarcodes() throws IOException {
+
+    public static ArrayList getBarcodes() throws IOException {
+        ArrayList arrayList = new ArrayList();
         FileReader read = new FileReader("src/main/java/com/thoughtworks/cart/cart.txt");
         BufferedReader br = new BufferedReader(read);
         String row;
+
         while((row = br.readLine()) != null) {
-            System.out.println(row);
+            arrayList.add(row);
         }
-
-        return null;
-
+        return arrayList;
     }
 
     public static void main(String[] args) throws IOException {
