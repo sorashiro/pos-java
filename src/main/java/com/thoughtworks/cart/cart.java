1809f0ccb510b22ba998ea01d9dcd49046492f9d
sorashiro <yufei930101@gmail.com>
2014-10-08 18:08:46 +0800

    add get barcodes function

--- src/main/java/com/thoughtworks/cart/cart.java
+++ src/main/java/com/thoughtworks/cart/cart.java
@@ -1,7 +1,24 @@
 package com.thoughtworks.cart;
 
-/**
- * Created by shiro on 10/8/14.
- */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class cart {
+    public static String getBarcodes() throws IOException {
+        FileReader read = new FileReader("src/main/java/com/thoughtworks/cart/cart.txt");
+        BufferedReader br = new BufferedReader(read);
+        String row;
+        while((row = br.readLine()) != null) {
+            System.out.println(row);
+        }
+
+        return null;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        getBarcodes();
+    }
 }
