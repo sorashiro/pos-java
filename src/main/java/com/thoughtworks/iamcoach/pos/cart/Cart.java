6cdd47de514e13a6d81e0f006375f12b5df304b2
sigh <wangximingde@163.com>
2014-10-09 15:14:57 +0800

    farmat code

--- src/main/java/com/thoughtworks/iamcoach/pos/cart/Cart.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/cart/Cart.java
@@ -6,16 +6,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Cart {
-    private static String CARTFILE = "src/main/java/resources/cart.txt";
-    public  ArrayList getBarcodes() throws IOException {
-        ArrayList arrayList = new ArrayList<String>();
-        FileReader read = new FileReader(CARTFILE);
-        BufferedReader br = new BufferedReader(read);
-        String row;
+  private static String CARTFILE = "src/main/java/resources/cart.txt";
 
-        while((row = br.readLine()) != null) {
-            arrayList.add(row);
-        }
-        return arrayList;
+  public ArrayList getBarcodes() throws IOException {
+    ArrayList arrayList = new ArrayList<String>();
+    FileReader read = new FileReader(CARTFILE);
+    BufferedReader br = new BufferedReader(read);
+    String row;
+
+    while ((row = br.readLine()) != null) {
+      arrayList.add(row);
     }
+    return arrayList;
+  }
+  
 }
