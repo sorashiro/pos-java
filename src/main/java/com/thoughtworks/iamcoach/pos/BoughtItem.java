37b82c095cc571bd97100a8a0c26a5493f1990f8
sigh <wangximingde@163.com>
2014-10-09 15:49:02 +0800

    update BoughtItem class

--- src/main/java/com/thoughtworks/iamcoach/pos/BoughtItem.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/BoughtItem.java
@@ -1,4 +1,30 @@
 package com.thoughtworks.iamcoach.pos;
 
 public class BoughtItem {
+  private String barcode;
+  private int number;
+
+  public BoughtItem(String barcode, int number) {
+    this.barcode = barcode;
+    this.number = number;
+  }
+  public String getBarcode() {
+    return barcode;
+  }
+
+  public void setBarcode(String barcode) {
+    this.barcode = barcode;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+
+  public void addItemNumberByOne() {
+    this.number++;
+  }
 }
