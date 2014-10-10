6fc0ec20153c2106d33871f6c38791225df8c5c6
sorashiro <yufei930101@gmail.com>
2014-10-10 18:01:43 +0800

    update DiscountItem Class

--- src/main/java/com/thoughtworks/iamcoach/pos/DiscountItem.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/DiscountItem.java
@@ -1,5 +1,27 @@
 package com.thoughtworks.iamcoach.pos;
 
 public class DiscountItem {
+    private String barcode;
+    private String type;
 
+    public DiscountItem(String barcode, String type) {
+        this.barcode = barcode;
+        this.type = type;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
