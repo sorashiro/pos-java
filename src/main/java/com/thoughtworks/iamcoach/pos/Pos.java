7bae1332ca204aba1f42984cd932ae500472ccb1
sorashiro <yufei930101@gmail.com>
2014-10-11 16:10:22 +0800

    add calculatePromotion function

--- src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
@@ -33,10 +33,29 @@ public class Pos {
     for(String cartItem : cartItems){
       for(DiscountItem promotion : promotions){
         if(cartItem.equals(promotion.getBarcode())){
-
+          calculatePromotion(promotion);
           
         }
       }
     }
   }
+
+  private void calculatePromotion(DiscountItem promotion) throws IOException {
+    ItemServer itemServer = new ItemServer();
+    Cart cart = new Cart();
+    BoughtItem boughtItem = cart.findItemNumber(promotion.getBarcode());
+    Item item = itemServer.findItem(promotion.getBarcode());
+
+    if(promotion.getType().equals("buy_two_get_one_free")) {
+      calculateBuyTwo(item, boughtItem);
+    }
+    else if(promotion.getType().equals("second_half_price")) {
+      calculateHalfPrice(item, boughtItem);
+    }
+    else if(promotion.getType().contains("discount")) {
+      calculateDiscount(item, boughtItem, promotion.getType());
+    }
+  }
+
+
 }
