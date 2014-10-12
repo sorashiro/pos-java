3db730904c1fd2f23a628a5616f09ed8cef9458f
sigh <wangximingde@163.com>
2014-10-12 14:43:08 +0800

    add get promotions test

--- src/test/java/com/thoughtworks/iamcoach/pos/DiscountTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/DiscountTest.java
@@ -1,4 +1,21 @@
 package com.thoughtworks.iamcoach.pos;
 
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class DiscountTest {
+  Discount discount = new Discount();
+
+  @Test
+  public void get_all_discount_promotions(){
+    try {
+      assertThat(discount.getPromotions().size()).isEqualTo(9);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
