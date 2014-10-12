047cbb63e3d765572d80a83d46db5fe5333536d4
sigh <wangximingde@163.com>
2014-10-12 14:55:46 +0800

    update get promotions test

--- src/test/java/com/thoughtworks/iamcoach/pos/DiscountTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/DiscountTest.java
@@ -3,8 +3,6 @@ package com.thoughtworks.iamcoach.pos;
 
 import org.junit.Test;
 
-import java.io.IOException;
-
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class DiscountTest {
@@ -12,10 +10,6 @@ public class DiscountTest {
 
   @Test
   public void get_all_discount_promotions(){
-    try {
       assertThat(discount.getPromotions().size()).isEqualTo(9);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
   }
 }
