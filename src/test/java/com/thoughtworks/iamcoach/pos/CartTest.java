d51e3a5bd1f1e939cebd084c7d7a0aef2a77c576
sigh <wangximingde@163.com>
2014-10-12 10:11:03 +0800

    add read cart file test

--- src/test/java/com/thoughtworks/iamcoach/pos/CartTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/CartTest.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.iamcoach.pos;
 
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class CartTest {
+
+  @Test
+  public void cart_file_should_be_read_correct(){
+    Cart cart = new Cart();
+    assertThat(cart.getBarcodes().size()).isEqualTo(9);
+  }
 }
