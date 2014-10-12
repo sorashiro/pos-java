712aafbbcd96b3fbcfb213d9680ed24e8789547e
sigh <wangximingde@163.com>
2014-10-12 11:09:03 +0800

    update handle barcodes test

--- src/test/java/com/thoughtworks/iamcoach/pos/PosTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PosTest.java
@@ -12,12 +12,17 @@ public class PosTest {
   Pos pos = new Pos();
   @Test
   public  void handle_barcodes_test(){
-    List<BoughtItem> boughtItemList = new ArrayList<BoughtItem>();
-    boughtItemList.add(new BoughtItem("ITEM000001", 1.00));
     List<String> cartBarcodes = new ArrayList<String>();
     cartBarcodes.add("ITEM000001");
+    cartBarcodes.add("ITEM000001");
+    cartBarcodes.add("ITEM000001");
+    cartBarcodes.add("ITEM000001");
+
     try {
-      assertThat(pos.handleBarcodes(cartBarcodes)).isEqualTo(boughtItemList);
+      List<BoughtItem> boughtItemList = pos.handleBarcodes(cartBarcodes);
+      BoughtItem boughtItem = boughtItemList.get(0);
+      assertThat(boughtItem.getNumber()).isEqualTo(4.00);
+      assertThat(boughtItem.getBarcode()).isEqualTo("ITEM000001");
     } catch (IOException e) {
       e.printStackTrace();
     }
