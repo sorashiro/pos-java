5a9093533bce83bfbf4006885b00a4e0d2b2e7af
sigh <wangximingde@163.com>
2014-10-12 15:43:00 +0800

    move textToList method

--- src/main/java/com/thoughtworks/iamcoach/pos/Cart.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Cart.java
@@ -1,35 +1,27 @@
 package com.thoughtworks.iamcoach.pos;
 
+import com.thoughtworks.iamcoach.pos.util.FileUtil;
+
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
   private static final String CART_FILE = "src/main/resources/cart.txt";
 
   public List<String> getBarcodes(){
-    Path cartFile = Paths.get(CART_FILE);
-    List<String> linesRead = null;
-    try {
-      linesRead = Files.readAllLines(cartFile);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    List<String> linesRead = FileUtil.textToList(CART_FILE);;
     return linesRead;
   }
 
   public BoughtItem findItemNumber(String  barcode) throws IOException {
     Pos pos = new Pos();
     BoughtItem result = new BoughtItem();
-    ArrayList<BoughtItem> boughtItems = pos.countCart();
-    for(BoughtItem boughtItem : boughtItems ){
-        if (barcode.equals(boughtItem.getBarcode())){
-            result = boughtItem;
-        }
-    }
+//    ArrayList<BoughtItem> boughtItems = pos.countCart();
+//    for(BoughtItem boughtItem : boughtItems ){
+//        if (barcode.equals(boughtItem.getBarcode())){
+//            result = boughtItem;
+//        }
+//    }
       return  result;
   }
 
