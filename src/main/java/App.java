854ab04d8424d0d842825c96ebd9a8b52b8f6932
sigh <wangximingde@163.com>
2014-10-12 13:33:36 +0800

    update App class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,19 @@
+import com.thoughtworks.iamcoach.pos.BoughtItem;
+import com.thoughtworks.iamcoach.pos.Cart;
+import com.thoughtworks.iamcoach.pos.Pos;
+
+import java.io.IOException;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        //TODO: Need to  implement.
+      Cart cart = new Cart();
+      Pos pos = new Pos();
+      List<String> cartBarcodes = cart.getBarcodes();
+      try {
+        List<BoughtItem> boughtItems = pos.handleBarcodes(cartBarcodes);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
 }
