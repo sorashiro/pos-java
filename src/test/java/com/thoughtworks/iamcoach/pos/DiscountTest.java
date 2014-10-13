ba8f8f134794015fd6caba200017d1b7fdf8bf46
sorashiro <yufei930101@gmail.com>
2014-10-13 16:24:38 +0800

    add get_promotion_type test

--- src/test/java/com/thoughtworks/iamcoach/pos/DiscountTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/DiscountTest.java
@@ -12,4 +12,10 @@ public class DiscountTest {
   public void get_all_discount_promotions() {
     assertThat(discount.getPromotions().size()).isEqualTo(9);
   }
+
+  @Test
+  public void get_promotion_type() {
+    String barcode = "ITEM000001";
+    assertThat(discount.getPromotionType(barcode)).isEqualTo("buy_two_get_one_free");
+  }
 }
