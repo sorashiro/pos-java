e25494082d9b46b4ea20d26bb4422dddce8cb814
sigh <wangximingde@163.com>
2014-10-13 20:02:34 +0800

    delete useless code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,5 @@
 import com.thoughtworks.iamcoach.pos.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class App {
@@ -9,13 +8,7 @@ public class App {
     Pos pos = new Pos();
     Discount discount = new Discount();
     List<String> cartBarcodes = cart.getBarcodes();
-    List<BoughtItem> boughtItems = new ArrayList<BoughtItem>();
-
-
-      boughtItems = pos.handleBarcodes(cartBarcodes);
-
-
-
+    List<BoughtItem> boughtItems = pos.handleBarcodes(cartBarcodes);
     List<Promotion> promotions = discount.getPromotions();
       pos.calculatePromotion(boughtItems);
 
