55b2fcb05cbe726f33aa1c154c37acb3508ff683
sorashiro <yufei930101@gmail.com>
2014-10-13 15:12:34 +0800

    get bought items by cart items

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,23 @@
-import com.thoughtworks.iamcoach.pos.BoughtItem;
-import com.thoughtworks.iamcoach.pos.Cart;
-import com.thoughtworks.iamcoach.pos.Pos;
+import com.thoughtworks.iamcoach.pos.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class App {
   public static void main(String[] args) {
+    ItemServer itemServer = new ItemServer();
     Cart cart = new Cart();
     Pos pos = new Pos();
     List<String> cartBarcodes = cart.getBarcodes();
     try {
-      List<BoughtItem> boughtItems = pos.handleBarcodes(cartBarcodes);
+      List<BoughtItem> cartItems = pos.handleBarcodes(cartBarcodes);
+      List<Item> boughtItems = new ArrayList<Item>();
+      for (BoughtItem cartItem : cartItems) {
+        Item item = itemServer.findItem(cartItem.getBarcode());
+        boughtItems.add(item);
+      }
+      
     } catch (IOException e) {
       e.printStackTrace();
     }
