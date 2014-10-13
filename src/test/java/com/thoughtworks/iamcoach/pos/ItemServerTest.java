9c2c2a672ee43d8539230a81c340272f7ba99301
sorashiro <yufei930101@gmail.com>
2014-10-13 17:30:57 +0800

    add findItem test

--- src/test/java/com/thoughtworks/iamcoach/pos/ItemServerTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/ItemServerTest.java
@@ -1,4 +1,22 @@
 package com.thoughtworks.iamcoach.pos;
 
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class ItemServerTest {
+    ItemServer itemServer = new ItemServer();
+    @Test
+    public void find_item_test() {
+        String barcode = "ITEM000001";
+        Item item = new Item("ITEM000001", "可口可乐", "瓶", 3.50);
+        try {
+            assertThat(itemServer.findItem(barcode).getBarcode()).isEqualTo("ITEM000001");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
