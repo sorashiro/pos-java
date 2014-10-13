527bc4c0bcf23b7f646b201012ae2e3c40ea6310
sorashiro <yufei930101@gmail.com>
2014-10-13 16:57:22 +0800

    add calculate_promotion_test

--- src/test/java/com/thoughtworks/iamcoach/pos/PosTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PosTest.java
@@ -35,4 +35,16 @@ public class PosTest {
       e.printStackTrace();
     }
   }
+
+  @Test
+  public void calculate_promotion_test() {
+    List<BoughtItem> boughtItems = new ArrayList<BoughtItem>();
+    Item item = new Item("ITEM000002", "apple", "kg", 10.00);
+    boughtItems.add(new BoughtItem(item, 4.00));
+      try {
+          assertThat(pos.calculatePromotion(boughtItems)).isEqualTo(30.00);
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+  }
 }
