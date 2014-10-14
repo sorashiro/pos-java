ba1ab4304c80f62aaabcc9903f307c4ae20a5c27
sigh <wangximingde@163.com>
2014-10-14 16:25:45 +0800

    update FileUtil

--- src/main/java/com/thoughtworks/iamcoach/pos/util/FileUtil.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/util/FileUtil.java
@@ -9,12 +9,14 @@ import java.util.List;
 public class FileUtil {
   public static List<String> textToList(String path) {
     Path file = Paths.get(path);
+
     List<String> linesRead = null;
     try {
       linesRead = Files.readAllLines(file);
     } catch (IOException e) {
       e.printStackTrace();
     }
+    
     return linesRead;
   }
 }
