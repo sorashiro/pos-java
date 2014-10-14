eb816088b01e8cbf45b93800d5ecad7cfc73b307
sorashiro <yufei930101@gmail.com>
2014-10-14 17:23:37 +0800

    update PrintItem Class

--- src/main/java/com/thoughtworks/iamcoach/pos/PrintItem.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/PrintItem.java
@@ -1,4 +1,27 @@
 package com.thoughtworks.iamcoach.pos;
 
 public class PrintItem {
+    private BoughtItem boughtItem;
+    private Double subtotal;
+
+    public PrintItem(BoughtItem boughtItem, Double subtotal) {
+        this.setBoughtItem(boughtItem);
+        this.setSubtotal(subtotal);
+    }
+
+    public BoughtItem getBoughtItem() {
+        return boughtItem;
+    }
+
+    public void setBoughtItem(BoughtItem boughtItem) {
+        this.boughtItem = boughtItem;
+    }
+
+    public Double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(Double subtotal) {
+        this.subtotal = subtotal;
+    }
 }
