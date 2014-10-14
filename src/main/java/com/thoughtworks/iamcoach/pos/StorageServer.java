558ebea8fd920f0061df1bdafcd893b946913d0e
sorashiro <yufei930101@gmail.com>
2014-10-14 16:03:21 +0800

    success get_items_test

--- src/main/java/com/thoughtworks/iamcoach/pos/StorageServer.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/StorageServer.java
@@ -1,4 +1,25 @@
 package com.thoughtworks.iamcoach.pos;
 
+import com.thoughtworks.iamcoach.pos.util.FileUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StorageServer {
+    private static final String ITEM_FILE = "src/main/resources/item.txt";
+
+    public List<Item> getItems() {
+        List<Item> arrayList = new ArrayList<Item>();
+
+        List<String> linesRead = FileUtil.textToList(ITEM_FILE);
+
+        for (String line : linesRead) {
+            String[] stringItem = line.split(" ");
+            Item item = new Item(stringItem[0], stringItem[1], stringItem[2], Double.parseDouble(stringItem[3]));
+            arrayList.add(item);
+        }
+        return arrayList;
+    }
+
+
 }
--- src/test/java/com/thoughtworks/iamcoach/pos/StorageServerTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/StorageServerTest.java
@@ -1,4 +1,15 @@
 package com.thoughtworks.iamcoach.pos;
 
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+
 public class StorageServerTest {
+
+    @Test
+    public void get_items_test() {
+        StorageServer storageServer = new StorageServer();
+        assertThat(storageServer.getItems().size()).isEqualTo(9);
+    }
 }
