caf459112c7bff735cda9c02e571ff7a6f55f528
sorashiro <yufei930101@gmail.com>
2014-10-14 16:05:41 +0800

    delete unuseful code

--- src/main/java/com/thoughtworks/iamcoach/pos/ItemServer.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/ItemServer.java
@@ -1,28 +1,12 @@
 package com.thoughtworks.iamcoach.pos;
 
-import com.thoughtworks.iamcoach.pos.util.FileUtil;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class ItemServer {
-  private static final String ITEM_FILE = "src/main/resources/item.txt";
 
-  private List<Item> getItems() {
-    List<Item> arrayList = new ArrayList<Item>();
-
-    List<String> linesRead = FileUtil.textToList(ITEM_FILE);
-
-    for (String line : linesRead) {
-      String[] stringItem = line.split(" ");
-      Item item = new Item(stringItem[0], stringItem[1], stringItem[2], Double.parseDouble(stringItem[3]));
-      arrayList.add(item);
-    }
-    return arrayList;
-  }
-
+    StorageServer storageServer = new StorageServer();
   public Item findItem(String barcode){
-    List<Item> items = getItems();
+    List<Item> items = storageServer.getItems();
     Item result = new Item();
     for (Item item : items) {
       if (barcode.equals(item.getBarcode())) {
