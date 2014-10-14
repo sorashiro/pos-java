7b123e27e21fb0f7e6471d1c924a8529192c53b8
sorashiro <yufei930101@gmail.com>
2014-10-14 15:42:24 +0800

    delete unuseful code

--- src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
@@ -23,23 +23,6 @@ public class Pos {
     return boughtItemList;
   }
 
-  public Double calculatePromotion(List<BoughtItem> boughtItems){
-    PromotionServer promotionServer = new PromotionServer();
-    Double result = 0.00;
-    for (BoughtItem boughtItem : boughtItems) {
-      String barcode = boughtItem.getBarcode();
-      String promotionType = promotionServer.getPromotionType(barcode);
-      if (promotionType.equals("buy_two_get_one_free")) {
-        result += calculateBuyTwo(boughtItem);
-      } else if (promotionType.equals("second_half_price")) {
-        result += calculateHalfPrice(boughtItem);
-      } else if (promotionType.contains("discount")) {
-        result += calculateDiscount(boughtItem, promotionType);
-      }
-    }
-    return result;
-  }
-
   private List<String> uniqueArray(List<String> cartBarcodes) {
     List<String> tempArray = new ArrayList<String>();
     for (String barcode : cartBarcodes) {
@@ -58,26 +41,5 @@ public class Pos {
       }
     }
     return result;
-  }
-
-  private Double calculateBuyTwo(BoughtItem boughtItem) {
-    Double number = boughtItem.getNumber();
-    Double payNumber = number - (int)(number / 3);
-
-    return boughtItem.getPrice() * payNumber;
-  }
-
-  private Double calculateHalfPrice(BoughtItem boughtItem) {
-    Double number = boughtItem.getNumber();
-    Double total = boughtItem.getPrice() * number;
-    Double discountTotal = (0.5 * boughtItem.getPrice()) * (int)(number / 2);
-
-    return total - discountTotal;
-  }
-
-  private Double calculateDiscount(BoughtItem boughtItem, String typeInfo) {
-    Double number = boughtItem.getNumber();
-    Double discount = Double.parseDouble(typeInfo.split(":")[1]);
-    return boughtItem.getPrice() * number * discount / 100;
   }
 }
