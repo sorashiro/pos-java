e44d193c0d7b66e4d92183f2ff9ebce1a3d76bda
sorashiro <yufei930101@gmail.com>
2014-10-14 18:19:43 +0800

    success printItemList method

--- src/main/java/Output.java
+++ src/main/java/Output.java
@@ -1,17 +1,28 @@
 import com.thoughtworks.iamcoach.pos.PrintItem;
 
+import java.util.Date;
 import java.util.List;
 
 public class Output {
     public static void printShoppingList(List<PrintItem> printItemList) {
-        String shoppingList = "";
+        Date date = new Date();
+        String shoppingList = "**********沃尔玛超市**********\n";
+        //TODO need to replace with guava;
+        shoppingList += date + "\n\n";
+        Double total = 0.00;
+        Double sum = 0.00;
         for (PrintItem printItem : printItemList) {
             shoppingList += printItem.getBoughtItem().getItem().getName() + " ";
             shoppingList += printItem.getBoughtItem().getNumber();
             shoppingList += printItem.getBoughtItem().getItem().getUnit() + " ";
             shoppingList += printItem.getBoughtItem().getPrice() + "元 ";
             shoppingList += printItem.getSubtotal() + "元\n";
+            total += printItem.getSubtotal();
+            sum += printItem.getBoughtItem().getNumber() * printItem.getBoughtItem().getPrice();
         }
+        shoppingList += "优惠前金额:" + sum + "元 \n";
+        shoppingList += "优惠后金额:" + total + "元 \n";
+        shoppingList += "优惠差价:" + (sum - total) + "元";
 
         System.out.println(shoppingList);
     }
