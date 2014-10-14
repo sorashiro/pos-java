594cb234c75388f8279da4e3a0ea668cbcd646b8
sorashiro <yufei930101@gmail.com>
2014-10-14 16:11:37 +0800

    delete Cart Class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class App {
   public static void main(String[] args) {
 
-    Cart cart = new Cart();
-    List<String> cartBarcodes = cart.getBarcodes();
+    StorageServer storageServer = new StorageServer();
+    List<String> cartBarcodes = storageServer.getCartBarcodes();
 
     Pos pos = new Pos();
     List<BoughtItem> boughtItems = pos.handleBarcodes(cartBarcodes);
