dcfc4d7328955f0eca3a770b7ee72ed688bdadba
sigh <wangximingde@163.com>
2014-10-14 16:42:01 +0800

    update handleBarcodes method

--- src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
@@ -7,19 +7,12 @@ public class Pos {
 
   public List<BoughtItem> handleBarcodes(List<String> cartBarcodes) {
     List<BoughtItem> boughtItemList = new ArrayList<BoughtItem>();
-    ItemServer itemServer = new ItemServer();
     List<String> uniqueBarcodes = uniqueArray(cartBarcodes);
 
     for (String uniqueBarcode : uniqueBarcodes) {
       int times = getBarcodeTimes(cartBarcodes, uniqueBarcode);
-      String[] barcodes = uniqueBarcode.split("-");
-      Double number = 1.00;
-
-      if (barcodes.length == 2) {
-        number = Double.parseDouble(barcodes[1]);
-      }
-
-      boughtItemList.add(new BoughtItem(itemServer.findItem(barcodes[0]), number * times));
+      BoughtItem boughtItem = toBoughtItem(uniqueBarcode, times);
+      boughtItemList.add(boughtItem);
     }
     return boughtItemList;
   }
