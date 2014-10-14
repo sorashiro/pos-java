f23bf362467472ff0e9f4aa4f1ac66e5ea9d686b
sorashiro <yufei930101@gmail.com>
2014-10-14 15:46:16 +0800

    update code

--- src/main/java/com/thoughtworks/iamcoach/pos/ItemServer.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/ItemServer.java
@@ -12,11 +12,11 @@ public class ItemServer {
     List<Item> arrayList = new ArrayList<Item>();
 
     List<String> linesRead = FileUtil.textToList(ITEM_FILE);
+
     for (String line : linesRead) {
       String[] stringItem = line.split(" ");
       Item item = new Item(stringItem[0], stringItem[1], stringItem[2], Double.parseDouble(stringItem[3]));
       arrayList.add(item);
-
     }
     return arrayList;
   }
