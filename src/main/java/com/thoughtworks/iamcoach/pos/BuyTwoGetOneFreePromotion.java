4e3f779675dc8ab203edcc465ba83d40f4c4b7ed
sigh <wangximingde@163.com>
2014-10-15 13:29:25 +0800

    make BuyTwoGetOneFreePromotion extends PromotionType

--- src/main/java/com/thoughtworks/iamcoach/pos/BuyTwoGetOneFreePromotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/BuyTwoGetOneFreePromotion.java
@@ -1,4 +1,8 @@
 package com.thoughtworks.iamcoach.pos;
 
-public class BuyTwoGetOneFreePromotion {
+public class BuyTwoGetOneFreePromotion extends PromotionType{
+  @Override
+  String getPromotionType() {
+    return PromotionServer.BUY_TWO_GET_ONE_FREE;
+  }
 }
