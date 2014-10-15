321fdef9b4c8d2b05fa0e2bc99b4a0097a84fae4
sigh <wangximingde@163.com>
2014-10-15 13:31:45 +0800

    make SecondhalfPricePromotion extends PromotionType

--- src/main/java/com/thoughtworks/iamcoach/pos/SecondHalfPricePromotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/SecondHalfPricePromotion.java
@@ -1,4 +1,8 @@
 package com.thoughtworks.iamcoach.pos;
 
-public class SecondHalfPricePromotion {
+public class SecondHalfPricePromotion extends PromotionType{
+  @Override
+  String getPromotionType() {
+    return PromotionServer.SECOND_HALF_PRICE;
+  }
 }
