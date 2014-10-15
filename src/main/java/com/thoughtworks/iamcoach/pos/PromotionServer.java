e85ba2d0f915603ed5255a7d33f521eac7e26a88
sigh <wangximingde@163.com>
2014-10-15 15:01:29 +0800

    delete useless code

--- src/main/java/com/thoughtworks/iamcoach/pos/PromotionServer.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/PromotionServer.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 public class PromotionServer {
   private PromotionType promotionType;
-  public String getPromotionType() {
-    return promotionType.getPromotionType();
-  }
 
   public void setPromotionType(String barcode){
     promotionType = PromotionType.newType(barcode);
@@ -24,35 +21,8 @@ public class PromotionServer {
     return printItemList;
   }
 
-
   private PrintItem doCalculate(BoughtItem boughtItem){
     return promotionType.doCalculate(boughtItem);
   }
 
-  private PrintItem calculateBoughtItem(BoughtItem boughtItem) {
-    Double subtotal = boughtItem.getPrice() * boughtItem.getNumber();
-    return new PrintItem(boughtItem, subtotal);
-  }
-  private PrintItem calculateBuyTwo(BoughtItem boughtItem) {
-
-    Double number = boughtItem.getNumber();
-    Double payNumber = number - (int) (number / 3);
-    Double subtotal = boughtItem.getPrice() * payNumber;
-    return new PrintItem(boughtItem, subtotal);
-  }
-
-  private PrintItem calculateHalfPrice(BoughtItem boughtItem) {
-    Double number = boughtItem.getNumber();
-    Double total = boughtItem.getPrice() * number;
-    Double discountTotal = (0.5 * boughtItem.getPrice()) * (int) (number / 2);
-    Double subtotal = total - discountTotal;
-    return new PrintItem(boughtItem, subtotal);
-  }
-
-  private PrintItem calculateDiscount(BoughtItem boughtItem, String typeInfo) {
-    Double number = boughtItem.getNumber();
-    Double discount = Double.parseDouble(typeInfo.split(":")[1]);
-    Double subtotal = boughtItem.getPrice() * number * discount / 100;
-    return new PrintItem(boughtItem, subtotal);
-  }
 }
